9b75f53b7b6fbf1aba6b51c58587b90f0843e1d8
unknown <tnorth93@gmail.com>
2019-01-18 11:01:29 -0800

    playing with state

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -17,11 +17,30 @@ class App extends Component {
     ],
   };
 
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {
+          name: 'Thomas',
+          age: 25,
+        },
+        {
+          name: 'Kelly',
+          age: 26,
+        },
+        {
+          name: 'Beans',
+          age: 21,
+        }
+      ]
+    })
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button>Switch Name!</button>
+        <button onClick={this.switchNameHandler}>Switch Name!</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} > YOLO </Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > YOLO </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} > YOLO </Person>
