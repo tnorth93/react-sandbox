3376e00e3c2b91543e26119ccb841994dd4fdc10
unknown <tnorth93@gmail.com>
2019-01-18 12:04:04 -0800

    userInput component added

---
+++ basic-syntax-assignment/src/components/UserInput/UserInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userInput = (props) => {
+  return (
+    <div className="userInput">
+      <input type="text"/>
+    </div>
+
+  )
+};
+
+  export default userInput;
