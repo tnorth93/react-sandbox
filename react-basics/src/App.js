0b2488dfea9723dd3984e09bda76ac6548375aaa
unknown <tnorth93@gmail.com>
2019-01-24 15:49:47 -0800

    list populating dynamically

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -77,17 +77,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} > child </Person>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            changed={this.nameChangedHandler}> second child </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            click={this.switchNameHandler.bind(this, 'Tom!')} > third child </Person>
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age} />
+          })}
         </div>
       )
     }
