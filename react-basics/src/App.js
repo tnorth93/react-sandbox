baa70618baee27c2edee33d528fe9460f8177b6d
unknown <tnorth93@gmail.com>
2019-01-24 15:32:30 -0800

    changed persons jsx for clarity

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -72,28 +72,33 @@ class App extends Component {
       cursor: 'pointer',
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} > child </Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            changed={this.nameChangedHandler}> second child </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+            click={this.switchNameHandler.bind(this, 'Tom!')} > third child </Person>
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <button
           style={style}
           onClick={() => this.togglePersonsHandler()}>Switch Name!</button>
-        {
-          this.state.showPersons ?
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age} > child </Person>
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                changed={this.nameChangedHandler}> second child </Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}
-                click={this.switchNameHandler.bind(this, 'Tom!')} > third child </Person>
-            </div> : null
-        }
+        {persons}
       </div>
     );
   }
