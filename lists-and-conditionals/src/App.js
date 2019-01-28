e8e275371b54ccc13cb7f358f5c5f1b77fa09f8a
unknown <tnorth93@gmail.com>
2019-01-28 10:47:13 -0800

    dynamic message based on input length is working

--- lists-and-conditionals/src/App.js
+++ lists-and-conditionals/src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import Validation from './components/validation';
 
 class App extends Component {
   state = {
     inputText: '',
+    inputValidation: false,
   };
 
   textChangeHandler = (event) => {
-    let inputTextChange = event.target.value;
-    this.setState( {inputText:inputTextChange} );
+    this.setState({inputText: event.target.value});
+    if (this.state.inputLength >= 5) {
+      let inputValidation = true;
+      this.setState({inputValidation: inputValidation})
+    }
   };
 
   render() {
     return (
       <div className="App">
-        <input type='text' onChange={this.textChangeHandler} />
-        <p>{this.state.inputText.length}</p>
+        <input type='text'
+               onChange={this.textChangeHandler}
+               value={this.state.inputText} />
+        <p>{this.state.inputText}</p>
+        <Validation inputLength={this.state.inputText.length}/>
       </div>
     );
   }
--- lists-and-conditionals/src/components/validation.js
+++ lists-and-conditionals/src/components/validation.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const validationComponent = (props) => {
+
   return (
     <div className='ValidationComponent'>
-
+      {
+        props.inputLength >= 5 ? <p>Text Long Enough!</p>:<p>Text Too Short!</p>
+      }
     </div>
   )
 };
+
+export default validationComponent;
