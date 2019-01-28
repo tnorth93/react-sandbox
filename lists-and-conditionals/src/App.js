ac2299f8c789a03a8c5320d2332df36f1412f6c4
unknown <tnorth93@gmail.com>
2019-01-28 10:02:12 -0800

    completed handler that detects changes to input and displays character length

--- lists-and-conditionals/src/App.js
+++ lists-and-conditionals/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    inputText: '',
+  };
+
+  textChangeHandler = (event) => {
+    let inputTextChange = event.target.value;
+    this.setState( {inputText:inputTextChange} );
+  };
+
   render() {
     return (
       <div className="App">
-        <input type='text' />
+        <input type='text' onChange={this.textChangeHandler} />
+        <p>{this.state.inputText.length}</p>
       </div>
     );
   }
