6a47a11c8f223bad25d5cb5c58e7d0b4d0e9441f
unknown <tnorth93@gmail.com>
2019-01-31 09:43:36 -0800

    employed css modules for styling

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.module.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -86,15 +86,15 @@ class App extends Component {
 
     const classes = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      classes.push(styles.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      classes.push(styles.bold);
     }
 
     return (
 
-      <div className="App">
+      <div className={styles.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>Hello</p>
         <button
