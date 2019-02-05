f91569c2fb52b3ad15cb1f12dbfc9431e355f2d4
unknown <tnorth93@gmail.com>
2019-02-05 10:10:16 -0800

    removed forced error in person.js

--- react-basics/src/components/Persons/Person/Person.js
+++ react-basics/src/components/Persons/Person/Person.js
@@ -2,12 +2,7 @@ import React from 'react';
 import styles from './Person.module.css';
 
 const person = (props) => {
-  const rnd = Math.random();
-
-  if (rnd > 0.7) {
-    throw new Error('Something went wrong');
-  }
-
+  
     return (
 
       <div className={styles.Person}>
