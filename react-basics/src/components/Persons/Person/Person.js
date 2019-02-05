4daeb4d2f569931aa8829a5cd841733b6e85b9a1
unknown <tnorth93@gmail.com>
2019-02-05 11:57:19 -0800

    added console logs to track component lifecycles

--- react-basics/src/components/Persons/Person/Person.js
+++ react-basics/src/components/Persons/Person/Person.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import styles from './Person.module.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[Person.js] Inside Constructor", props);
+  }
+
+  componentWillMount() {
+    console.log('[Person.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[Person.js] Inside componentDidMount()');
+  }
+
   render(){
+    console.log('[Person.js] Inside render()');
     return (
       <div className={styles.Person}>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
--- react-basics/src/components/Persons/Persons.js
+++ react-basics/src/components/Persons/Persons.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import Person from "./Person/Person";
 
 class Persons extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[Persons.js] Inside Constructor", props);
+  }
+
+  componentWillMount() {
+    console.log('[Persons.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[Persons.js] Inside componentDidMount()');
+  }
+
   render() {
+    console.log('[Persons.js] Inside render()');
     return this.props.persons.map( ( person, index ) => {
       return <Person
+        key={person.id}
         click={() => this.props.clicked(index)}
         name={person.name}
         age={person.age}
